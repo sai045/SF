8ebcab07757635becd68681475c5953752ef00d0
sai045 <saivarshith.aalasyam@tensorgo.com>
2025-07-24 12:33:22 +0530

    w

--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = `http://localhost:5001/api/users`;
+const API_URL = `https://solo-fit-server.onrender.com/api/users`;
 
 const apiClient = axios.create({ baseURL: API_URL });
 
